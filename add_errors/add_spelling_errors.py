1637ff0ab25381ba7994d4bc060031ba752d95c7
HongyuGong <annfirst1111@gmail.com>
2017-04-08 20:33:16 -0500

    Update add_spelling_errors.py

--- add_errors/add_spelling_errors.py
+++ add_errors/add_spelling_errors.py
@@ -17,4 +17,20 @@ output:
     Modified_Sentences - a list of sentences, with each being the input sentence
                          with one word from Words_List modified.
 '''
+from nltk import word_tokenize, pos_tag
+
+def getContentWords(list_of_str):
+    tag_set = ["JJ", "JJR", "JJS", "NN", "NNS", "NNP", "NNPS", "RB", "RBR", "RBS",\
+               "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    selected_word_list = []
+    for s in list_of_str:
+        text = word_tokenize(s)
+        word_pos_list = pos_tag(text)
+        words = [item[0] for item in word_pos_list if item[1] in tag_set]
+        selected_word_list.append(words)
+    return selected_word_list
+
+
+
+
 def modify_key_words(sentence, Words_List)
